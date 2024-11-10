74b1aee7bb52c97238643505c85b577abb9bb689
Ali.Stanikzay <ali.kawsar.stanikzay@iths.se>
2024-11-10 02:39:02 +0100

    Game application done

---
+++ src/main/java/org/example/tictactoe/GameApplication.java
@@ -0,0 +1,23 @@
+package org.example.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Welcome to tic-tac-toe");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
